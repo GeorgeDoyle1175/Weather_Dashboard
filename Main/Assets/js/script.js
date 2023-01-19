f16add5eb8151e0ac8fa75f7a52af1603b0af6d1
Patrick <doyle1175@gmail.com>
2023-01-19 16:25:03 -0500

    Updated Fetch to append temp, humidity, wind to html

--- Main/Assets/js/script.js
+++ Main/Assets/js/script.js
@@ -9,13 +9,27 @@ function getCityData(event){
   console.log(cityInputEl.value);
 
 
-let localQueryUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + searchInput + 'appid=71e8bde5a86d6d139c2bbca4d34ba268'
+let localQueryUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + searchInput + '&units=Imperial&appid=71e8bde5a86d6d139c2bbca4d34ba268'
 
-console.log(localQueryurl);
+console.log(localQueryUrl);
 
   fetch(localQueryUrl)
   .then(response => response.json())
-  .then(data => console.log(data))
+
+  .then(data => {
+    const temperature = data.main.temp;
+    const temperatureElement = document.getElementById('temperature');
+    temperatureElement.innerHTML = temperature + " F";
+
+    const humidity = data.main.humidity;
+    const humidityElement = document.getElementById('humidity');
+    humidityElement.innerHTML = humidity;
+
+    const windSpeed = data.wind.speed;
+    const windSpeedElement = document.getElementById('wind-speed');
+    windSpeedElement.innerHTML = windSpeed + " mph";
+  })
+
   .catch(error => console.error(error))
 }
 inputFormEl.addEventListener("submit", getCityData);
